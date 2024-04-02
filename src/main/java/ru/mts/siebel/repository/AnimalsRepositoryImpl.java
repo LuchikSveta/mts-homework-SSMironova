4459f1e965cbf6e7d03fa7b1c3e902fb71d7fd2a
smironova <smironova@tsconsulting.com>
2024-04-02 11:48:52 +0300

    HW-4: реализация новых методов для репозитория, добавление Unit тестов

--- src/main/java/ru/mts/siebel/repository/AnimalsRepositoryImpl.java
+++ src/main/java/ru/mts/siebel/repository/AnimalsRepositoryImpl.java
@@ -3,13 +3,14 @@ package ru.mts.siebel.repository;
 import ru.mts.siebel.api.repository.IAnimal;
 import ru.mts.siebel.api.repository.IAnimalsRepository;
 import ru.mts.siebel.exception.EmptyAnimalsException;
+import ru.mts.siebel.exception.InvalidAverageAgeException;
+import ru.mts.siebel.exception.InvalidAverageCostException;
+import ru.mts.siebel.exception.InvalidMaxAgeException;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AnimalsRepositoryImpl implements IAnimalsRepository {
 
@@ -42,13 +43,9 @@ public class AnimalsRepositoryImpl implements IAnimalsRepository {
         if (animals.size() == 0) {
             throw new EmptyAnimalsException();
         }
-        Map<String, LocalDate> animalsLeapYear = new HashMap<>();
-        for (final IAnimal animal : animals) {
-            if (animal.getBirthDate().isLeapYear()) {
-                animalsLeapYear.put(animal.getClass().getSimpleName() + " " + animal.getName(), animal.getBirthDate());
-            }
-        }
-        return animalsLeapYear;
+        return animals.stream()
+                .filter(animal -> animal.getBirthDate().isLeapYear())
+                .collect(Collectors.toMap(animal -> animal.getClass().getSimpleName() + " " + animal.getName(), IAnimal::getBirthDate));
     }
 
     @Override
@@ -56,19 +53,17 @@ public class AnimalsRepositoryImpl implements IAnimalsRepository {
         if (animals.size() == 0) {
             throw new EmptyAnimalsException();
         }
-        int maxAge = 0;
-        Map<IAnimal, Integer> animalsMaxAge = new HashMap<>();
-        Map<IAnimal, Integer> animalsOlderN = new HashMap<>();
-        for (final IAnimal animal : animals) {
-            int age = Period.between(animal.getBirthDate(), LocalDate.now()).getYears();
-            if (age > n) {
-                animalsOlderN.put(animal, age);
-            }
-            if (age >= maxAge) {
-                maxAge = age;
-                animalsMaxAge.put(animal, age);
-            }
-        }
+        Map<IAnimal, Integer> animalsMaxAge = animals.stream()
+                .filter(animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears() >=
+                        animals.stream()
+                                .mapToInt(a -> Period.between(a.getBirthDate(), LocalDate.now()).getYears())
+                                .max()
+                                .orElseThrow(InvalidMaxAgeException::new))
+                .collect(Collectors.toMap(animal -> animal, animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears()));
+
+        Map<IAnimal, Integer> animalsOlderN = animals.stream()
+                .filter(animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears() > n)
+                .collect(Collectors.toMap(animal -> animal, animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears()));
         if (animalsOlderN.size() == 0) {
             return animalsMaxAge;
         }
@@ -76,20 +71,52 @@ public class AnimalsRepositoryImpl implements IAnimalsRepository {
     }
 
     @Override
-    public Map<String, Integer> findDuplicate() {
+    public Map<String, List<IAnimal>> findDuplicate() {
         if (animals.size() == 0) {
             throw new EmptyAnimalsException();
         }
-        Map<String, Integer> animalsNumber = new HashMap<>();
-        for (final IAnimal animal : animals) {
-            String animalType = animal.getClass().getSimpleName();
-            if (animalsNumber.size() == 0 || animalsNumber.get(animalType) == null) {
-                animalsNumber.put(animalType, 1);
-            } else {
-                animalsNumber.put(animalType, animalsNumber.get(animalType) + 1);
-            }
+        return animals.stream()
+                .collect(Collectors.groupingBy(animal -> animal.getClass().getSimpleName()));
+    }
+
+    @Override
+    public Double findAverageAge() {
+        if (animals.size() == 0) {
+            throw new EmptyAnimalsException();
         }
-        return animalsNumber;
+        return animals.stream()
+                .mapToInt(animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears())
+                .average()
+                .orElseThrow(InvalidAverageAgeException::new);
+    }
+
+    @Override
+    public List<IAnimal> findOldAndExpensive() {
+        if (animals.size() == 0) {
+            throw new EmptyAnimalsException();
+        }
+        return animals.stream()
+                .filter(animal -> Period.between(animal.getBirthDate(), LocalDate.now()).getYears() > 5)
+                .filter(animal -> animal.getCost() >
+                        animals.stream()
+                                .mapToDouble(IAnimal::getCost)
+                                .average()
+                                .orElseThrow(InvalidAverageCostException::new))
+                .sorted(Comparator.comparing(IAnimal::getBirthDate))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> findMinConstAnimals() {
+        if (animals.size() == 0) {
+            throw new EmptyAnimalsException();
+        }
+        return animals.stream()
+                .sorted(Comparator.comparing(IAnimal::getCost))
+                .limit(3)
+                .sorted((o1, o2) -> -o1.getName().compareTo(o2.getName()))
+                .map(IAnimal::getName)
+                .collect(Collectors.toList());
     }
 
 }
